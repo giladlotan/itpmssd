abe6d9dd98af27b4c5ad164cdc7a11cc28eaf0a1
Gilad <root@ip-172-31-3-27.ec2.internal>
2015-10-23 01:42:30 +0000

    making an endpoint

---
+++ Week_7/intro_server_final.py
@@ -0,0 +1,15 @@
+
+from flask import Flask # here we import the Flask class
+app = Flask(__name__) # we create an instance of the Flask class -> '__name__'
+
+@app.route("/") # we use whats called a 'decorator' to tell Flask what URL should trigger our function
+def hello():
+    return "Hello World!" 
+
+
+# only runs if the script is executed directly from the Python interpreter (and not used as an imported module)
+if __name__ == "__main__":  
+
+    app.debug = True # we want to get error messages
+    app.run(host='0.0.0.0', port=8886) # make your endpoint publicly available 
+
